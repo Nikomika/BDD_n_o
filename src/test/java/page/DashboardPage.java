7f41e0552905cb17901cc81e9e29af387aec2a44
OlgaNiki <oliadrok@gmail.com>
2024-01-06 18:33:51 +0300

    Add positive test

---
+++ src/test/java/page/DashboardPage.java
@@ -0,0 +1,52 @@
+package page;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import data.DataHelper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+import lombok.val;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DashboardPage {
+    // к сожалению, разработчики не дали нам удобного селектора, поэтому так
+    private ElementsCollection cards = $$(".list__item div");
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+    public DashboardPage() {
+        heading.shouldBe(Condition.visible);
+    }
+
+    public int getCardBalance(DataHelper.CardInfo cardInfo) {
+        var text = cards.findBy(Condition.text(cardInfo.getCardNumber().substring(15))).getText();
+        return extractBalance(text);
+    }
+
+    public int getCardBalance(int index) {
+        var text = cards.get(index).getText();
+        return extractBalance(text);
+    }
+
+    public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
+        cards.findBy(Condition.attribute("data-test-id", cardInfo.getTestID())).$("button").click();
+        return new TransferPage();
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+}
---
+++ src/test/java/test/MoneyTransferTest.java
@@ -0,0 +1,57 @@
+package test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static data.DataHelper.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import data.DataHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import page.DashboardPage;
+import page.LoginPage;
+
+public class MoneyTransferTest {
+    DashboardPage dashboardPage;
+    DataHelper.CardInfo firstCardInfo;
+    DataHelper.CardInfo secondCardInfo;
+    int firstCardBalance;
+    int secondCardBalance;
+
+    @BeforeEach
+    void setup() {
+        var loginPage = open("http://localhost:9999/", LoginPage.class);
+        var authInfo = getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = getVerificationCode();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+        firstCardInfo = getFirstCardInfo();
+        secondCardInfo = getSecondCardInfo();
+        firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+    }
+
+    @Test
+    void shouldTransferFromFirstToSecond() {
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceFirstCard =  firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+    @Test
+    void shouldTransferFromSecondToFirst() {
+        var amount = generateValidAmount(secondCardBalance);
+        var expectedBalanceFirstCard = firstCardBalance + amount;
+        var expectedBalanceSecondCard = secondCardBalance - amount;
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), secondCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+}
